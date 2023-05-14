17fec26e5569ddef3c011df2a7d739ee4e7ab1c4
A. N. M. Mehbube Arman <MArman88@users.noreply.github.com>
2023-05-15 00:07:06 +0600

    initial upload

---
+++ index-js.js
@@ -0,0 +1,57 @@
+let list = [];
+var isOdd = false;
+
+function addComment(comment) {
+    let div = document.getElementById("list");
+    let p = document.createElement("p");
+    if (isOdd) {
+        isOdd = false;
+        p.className = "list-item-light-blue";
+    } else {
+        isOdd = true;
+        p.className = "list-item-light-white";
+    }
+    p.innerText = comment;
+    div.appendChild(p);
+}
+
+function addCommentsInlist() {
+    let text = document.getElementById("textarea");
+    if (!isNullOrEmpty(text.value)) {
+        addComment(text.value);
+    }
+
+    text.value = "";
+}
+
+function isNullOrEmpty(value) {
+    return (!value || value == undefined || value == "" || value.length == 0);
+}
+
+let btnSubmit = document.getElementById("btnSubmit");
+btnSubmit.onclick = addCommentsInlist;
+
+let btnDelete = document.getElementById("btnDelete");
+let txtConfirm = document.getElementById("txtConfirm");
+
+txtConfirm.addEventListener('keyup', function () {
+    let value = txtConfirm.value;
+    if (value == "REPOSITORY") {
+        btnDelete.classList.remove("disabled");
+    } else {
+        btnDelete.classList.add("disabled");
+    }
+});
+
+btnDelete.onclick = function () {
+    alert("Thanks for confirming. File is going to be deleted soon.");
+    txtConfirm.value = "";
+    btnDelete.classList.add("disabled");
+}
+
+function initiateComments() {
+    addComment("Oh God can save us from this.");
+    addComment("Our prayers are who are in the front line trying to save people in need from this disaster.")
+}
+
+initiateComments();
